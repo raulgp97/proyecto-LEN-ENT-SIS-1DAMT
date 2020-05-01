a846771287aa3c2606ad7764d876bdaf844cacb3
raulgp97 <57761438+raulgp97@users.noreply.github.com>
2020-05-01 17:05:12 +0200

    Implementacion función atras y documentación código
    He implementado una función que permite retroceder a la página anterior y también he documentado el código.

--- lm_ed_sis.js
+++ lm_ed_sis.js
@@ -1,29 +1,21 @@
-
-var enlace=document.URL;
-
-var array = enlace.split("/",9);
 /*
-* Descripción función home: Vuelve a la página de inicio.
-*
-*
-*
+* Descripción función home: Retrocede a la página de inicio.
 */
 function home(){
 
-   location.assign("./prueba.html");
-    
-}
-/*
-* Descripción función back: Vuelve a la página anterior.
-* @TODO: Implementar un metodo que ya hace esta función.
-*
-*
-*/
-function back(array,enlace){
-   
-    location.assign("./"+array[8]);
+    location.assign("./prueba.html");
+     
  
-}
+ }
+ /*
+ * Descripción función back: Retrocede a la página anterior.
+ */
+ function back(){
+    
+     window.history.back();
+  
+ }
+
 
 
 
