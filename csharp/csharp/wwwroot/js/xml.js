873a943d7084f3d134586355d65ca92ccda05470
raulgp97 <57761438+raulgp97@users.noreply.github.com>
2020-05-24 14:31:05 +0200

    Xml en Asp.Net Csharp
    He hecho la lectura y la exportancion del fichero xml.

---
+++ csharp/csharp/wwwroot/js/xml.js
@@ -0,0 +1,53 @@
+﻿var respuesta = lectura();
+
+switch (respuesta) {
+
+    case "importar":
+
+        xml = new XMLHttpRequest();
+
+        xml.open('GET', './informacion.xml', true);
+
+        xml.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+
+                window.alert(xml.responseText);
+            }
+        }
+
+        xml.send();
+
+        break;
+
+    case "exportar":
+
+        //var sentencia = "@ViewData['Exportar']";
+
+        
+
+        
+
+        break;
+
+    case null:
+
+        window.alert("Has salido de la ventana");
+}
+
+
+function lectura() {
+
+    var opcion;
+
+    do {
+
+        opcion = window.prompt("opciones\n" + "1. importar xml.\n" + "2. exportar xml.");
+
+    } while (opcion != "importar" && opcion != "exportar" && opcion != null);
+
+    return opcion;
+
+}
+
+
+
